9f2cf75e014632b59ceab79577ceb4e7f50564ea
const39 <const8539@gmail.com>
2021-01-08 15:22:55 +0100

    Doc

--- color.py
+++ color.py
@@ -1,28 +1,76 @@
+# Sets the color encoding which will be used:
+#   - True : use Truecolors (24 bits encoding) - default
+#   - False: use 256-colors (8 bits encoding)
 TrueColorsEnabled = True
 
 def _asTruecolorString(rgb):
+    """
+    Encode the given color as a Truecolor string
+
+    Parameters:
+        rgb (tuple): a tuple containing Red, Green and Blue information
+
+    Returns:
+        The encoded string
+    """
     return "2;{};{};{}".format(rgb[0], rgb[1], rgb[2]) 
 
 def _as256String(rgb):
+    """
+    Encode the given color as a 256-colors string
+
+    Parameters:
+        rgb (tuple): a tuple containing Red, Green and Blue information
+
+    Returns:
+        The encoded string
+    """
     redRatio = (0 if rgb[0] < 75 else (rgb[0] - 35) / 40) * 6 * 6
-    greenRatio = (0 if rgb[1] < 75 else (rgb[1] - 35) / 40) * 6
+    greenRatio = (0 if rgb[1] < 75 else (rgb[1] - 35) / 40) * 6 
     blueRatio = (0 if rgb[2] < 75 else (rgb[2] - 35) / 40) + 16
     return "5;{}".format(int(redRatio + greenRatio + blueRatio))
 
 def _extractRGBFromHex(hexCode):
-    hexCode = hexCode.lstrip('#')
+    """
+    Extract RGB information from an hexadecimal color code
+
+    Parameters:
+        hexCode (string): an hexadecimal color code
+
+    Returns:
+        A tuple containing Red, Green and Blue information
+    """
+    hexCode = hexCode.lstrip('#')   # Remove the '#' from the string
+
+    # Convert each byte into decimal, store it in a tuple and return 
     return tuple(int(hexCode[i:i+2], 16) for i in (0, 2, 4))
 
 def computeColor(hexCode = "", isBackground = False):
+    """
+    Encode a color from its hex code as the currently selected encoding (@see TrueColorsEnabled).
+    If the color is transparent, set 'hexCode' to an empty string.
+
+    Parameters:
+        hexCode (string): the color hexadecimal code
+        isBackground (bool): indicated if the color is used as background or foreground
+
+    Returns:
+        A color encoded as a string
+    """
+    # If no hex code, return no color/transparent color code. 
     if hexCode == "":
         return "\033[0m"
     
+    # Extract RGB info from hex code
     rgb = _extractRGBFromHex(hexCode)
+
+    # Encode color according to current setting
     if TrueColorsEnabled:
         color = _asTruecolorString(rgb)
     else:
         color = _as256String(rgb)
     
+    # Add encoding prefix according to isBackground bool
     if isBackground:
         prefix = "\033[48;" 
     else:
--- prompt.py
+++ prompt.py
@@ -4,63 +4,101 @@ from sys import argv
 import json
 
 def parseSettings():
+    """
+    Parse the JSON settings file and return settings as a dictionary.
+    """
+    # Default filename
     filename = "settings.json"
+    # If a settings file in given, use it
     if len(argv) > 1:
         filename = argv[1]
+    # Open the file
     file = open(filename, 'r')
+    # Return the parsed JSON as a dictionary or exit with error code if the file was not valid JSON 
     try:
         return json.load(file)
     except json.JSONDecodeError:
         print(filename, "is not a valid settings file in JSON format.")
         exit(1)
 
-def buildName(name, textColorHex, backColorHex, nextBackColorHex):
+def buildName(name, textColorHex, backColorHex, delimBackColorHex):
+    """
+    Build a string segment terminated by a delimiter.
     
-    delim = ""
+    Parameters:
+        name (string): the segment text, usually the directory name 
+        textColorHex (string): hexadecimal color code used for the text
+        backColorHex (string): hexadecimal color code used as the text background
+        delimBackColorHex (string): hexadecimal color code used as the delimiter background
+
+    Returns:
+        The string formatted with color codes.
+    """
+    delim = "" # Delimiter
     # wrappingGroup = "\\[{}{}\\]"    # PS1
     wrappingGroup = "{}{}"    # debug
 
+    # Generate color codes
     textColor = computeColor(textColorHex, False)
     backColor = computeColor(backColorHex, True)
     delimColor = computeColor(backColorHex, False)
-    nextBackColor = computeColor(nextBackColorHex, True)
+    delimBackColor = computeColor(delimBackColorHex, True)
     colorOff = computeColor()
 
+    # Format groups with color codes
     dirColorStr = wrappingGroup.format(backColor, textColor)
     dirStr = "\u00A0" + name + "\u00A0"
-    delimColorStr = wrappingGroup.format(nextBackColor, delimColor)
+    delimColorStr = wrappingGroup.format(delimBackColor, delimColor)
     resetColorStr = wrappingGroup.format(colorOff, colorOff)
 
+    # Put groups together
     return dirColorStr + dirStr + delimColorStr + delim + resetColorStr
 
-path = getcwd()
-settings = parseSettings()
+path = getcwd() # Get current working directory
+settings = parseSettings()  # Get settings from file
 
-TrueColorsEnabled = settings['truecolor']
+TrueColorsEnabled = settings['truecolor']   # Set color encoding according to settings
 
-splittedPath = path.split('/')
-splittedPath.pop(0)
+splittedPath = path.split('/')  # Split path in list
+splittedPath.pop(0) # Remove blank element from list
 
+# Init indices and prompt
 pathIndex, textColorIndex, backColorIndex = 0, 0, 0
 finalPrompt = ""
+
+# Build a segment for each directory 
 while pathIndex < len(splittedPath):
 
+    # If it's the last element of the path, no background color for the delimiter
     if pathIndex == len(splittedPath) - 1:
-        nextBackColorHex = ""
+        delimBackColorHex = ""
+    # Else, get the next background color index for the delimiter background color
     else:
-        nextBackColorIndex = 0 if len(settings['background']) == backColorIndex + 1 else backColorIndex + 1 
-        nextBackColorHex = settings['background'][nextBackColorIndex]
+        # If we've reached the end of the background colors list, loop back to the start
+        if len(settings['background']) == backColorIndex + 1:
+            index = 0 
+        # Otherwise, use the next color in the list
+        else:
+            index = backColorIndex + 1 
 
-    finalPrompt += buildName(splittedPath[pathIndex], settings['foreground'][textColorIndex], settings['background'][backColorIndex], nextBackColorHex)
+    # Append the new segment to the prompt
+    finalPrompt += buildName(name=splittedPath[pathIndex],
+                             textColorHex=settings['foreground'][textColorIndex],
+                             backColorHex=settings['background'][backColorIndex],
+                             delimBackColorHex=settings['background'][index])
 
+    # If we've reached the end of the foreground colors list, loop back to the start
     if textColorIndex + 1 >= len(settings['foreground']):
         textColorIndex = -1
 
+    # If we've reached the end of the background colors list, loop back to the start
     if backColorIndex + 1 >= len(settings['background']):
         backColorIndex = -1
 
+    # Increment indices
     pathIndex += 1
     textColorIndex += 1
     backColorIndex += 1
 
+# Print the whole prompt
 print(finalPrompt, end="")
